86d65d96b425b3db264c4563dde76a2c8eb8fc8b
Kyle Weller <kywe665@gmail.com>
2012-08-22 00:29:34 -0600

    main page is in a good spot. Title in place. Transition to show resume smooth. Auto corrects font-size of title for screen size

--- browser/lib/browser.js
+++ browser/lib/browser.js
@@ -26,7 +26,30 @@
     });
     $('.css-container').on('.js-view-profile', 'click', function(){
       $('.js-overview').toggleClass('css-mini-overview');
+      $('.js-details').toggleClass('css-faded');
     });
+    sizeTitle();
+  }
+  
+  function sizeTitle() {
+    var size
+      , desiredWidth = $('.js-title').width()
+      , initial = $('.js-resizer').width()
+      ;
+    console.log('desire ' + desiredWidth);
+//    size = parseInt($('.js-resizer').css('font-size'), 10) - 1;
+    console.log('current', $('.js-resizer').width());
+ //   console.log('size', size);
+    while($('.js-resizer').width() > desiredWidth - 32) {
+      size = parseInt($('.js-resizer').css('font-size'), 10) - 1;
+      console.log('Newsize', size);
+      $('.js-resizer').css('font-size', size+'px');
+    }
+    console.log('size', size);
+    $('.js-title').css('font-size', (size-3)+'px');
+    if(initial > desiredWidth - 32){
+      $('.js-main-intro').css('padding-left', '16%'); 
+    }
   }
 
   domReady(init);
