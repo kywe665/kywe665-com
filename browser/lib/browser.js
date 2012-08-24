e80084fb66cae9704cf72a039bd58ac06a515cd0
Kyle Weller <kywe665@gmail.com>
2012-08-24 03:36:51 -0600

    New format layout in place. Main page functional. Sticky Footers and auto centering content!

--- browser/lib/browser.js
+++ browser/lib/browser.js
@@ -6,45 +6,25 @@
   "use strict";
 
   var $ = require('ender')
-    , _ = require('underscore')
     , window = require('window')
     , domReady = require('domready')
-    , pure = require('pure').$p
     , request = require('ahr2')
-    , forEachAsync = require('forEachAsync')
-    , serializeForm = require('serialize-form')
     , navigator = window.navigator
     ;
 
   function init() {
-    $('.css-container').on('.css-nav span', 'click', function(){
-      var newSection = $(this).attr('data-active')
-        , currentSection = $('.js-container').attr('data-active')
-        ;
-      $('.js-container').attr('data-active', newSection);
-      $('.js-'+newSection).removeClass('css-hidden');
-      $('.js-'+currentSection).addClass('css-hidden');
-      if(newSection === 'contact') {
-        injectMap();
-      }
+    $(window).resize(function() {
+      sizeContainer();
     });
-    $('.css-container').on('.js-view-profile', 'click', function(){
+    $('body').on('.js-view-profile', 'click', function(){
       $('.js-overview').toggleClass('css-mini-overview');
       $('.js-details').toggleClass('css-faded');
     });
-    $('.css-container').on('.js-get-directions', 'click', function(){
+    $('body').on('.js-get-directions', 'click', function(){
       console.log('click');
       mapLocation();
     });
-    sizeTitle();
-  }
-  
-  function injectMap() {
-    if($('.js-map-container .js-map').html().length < 1) {
-      console.log('injectingMap');
-      var iframe = '<iframe width="100%" height="300px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=637+N+300+E+Provo&amp;aq=&amp;sll=39.499761,-111.547028&amp;sspn=5.204832,9.876709&amp;ie=UTF8&amp;hq=&amp;hnear=637+N+300+E,+Provo,+Utah+84606&amp;t=m&amp;ll=40.250184,-111.653109&amp;spn=0.022928,0.036478&amp;z=14&amp;iwloc=A&amp;output=embed"></iframe>';
-      $('.js-map-container .js-map').html(iframe);
-    }
+    sizeContainer(true);
   }
   
   function mapLocation() {
@@ -67,22 +47,10 @@
     $('.js-generated-link').click();
   }
   
-  function sizeTitle() {
-    var size
-      , desiredWidth = $('.js-title').width()
-      , initial = $('.js-resizer').width()
-      ;
-    console.log('desire ' + desiredWidth);
-    console.log('current', $('.js-resizer').width());
-    while($('.js-resizer').width() > desiredWidth - 32) {
-      size = parseInt($('.js-resizer').css('font-size'), 10) - 1;
-      console.log('Newsize', size);
-      $('.js-resizer').css('font-size', size+'px');
-    }
-    console.log('size', size);
-    $('.js-title').css('font-size', (size-3)+'px');
-    if(initial > desiredWidth - 32){
-      $('.js-main-intro').css('padding-left', '16%'); 
+  function sizeContainer(first) {
+    var height = parseInt(window.innerHeight, 10) - 80;
+    if((height - 20) > parseInt($('.parent').attr('data-height'), 10)){
+      $('.parent').css('height', height+'px');
     }
   }
 
